1e1f73191ef8c1fa13f76fb3e8dcca870484cd0d
JimLinhx <90655300+JimLinhx@users.noreply.github.com>
2023-01-16 16:14:05 -0500

    Updated App.js with coursepage

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SignUp from './components/SignUp';
 import SignIn from './components/SignIn';
+import CourseHome from './pages/CourseHome';
+import Course from './pages/Course';
+import Lecture from './pages/Lecture';
 
 const navBar = (
   <div>
@@ -23,19 +26,16 @@ function App() {
           <Routes>
             <Route path='/SignUp' element={<SignUp />} />
             <Route path='/SignIn' element={<SignIn />} />
-            <Route path='/' index element={<Homepage />} />
+            <Route path='/Home' index element={<Homepage />} />
+            <Route path="/coursehome" element={<CourseHome />} />
+            <Route path="/coursehome/courses/:courseId" element={<Course />} />
+            <Route path="/coursehome/courses/:courseId/details/:detailsId" element={<Lecture />} />
           </Routes>
         </Router>
       </div>
     </div>
   );
 }
-
-
-
-
-
-export default App;
 
 
 
