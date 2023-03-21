a5a8b84c184081ef041be8e47c9451a3ffd2f31a
Chengyue Wei <weiyuezhong2012@gmail.com>
2023-03-21 12:02:09 -0400

    Profile page with basic CSS

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Styles/Profile.css';
+import axios from 'axios';
 
-function Profile() {
+const Profile = ({  }) => {
+  const [name, setName] = useState();
+  const [age, setAge] = useState();
+  const [bio, setBio] = useState();
+  const [profilePicture, setProfilePicture] = useState();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const userInfo = { name, age, bio, profilePicture };
+    try {
+      await axios.post('/api/users-info', userInfo);
+      alert('User information saved successfully!');
+    } catch (error) {
+      console.error(error);
+      alert('Error saving user information');
+    }
+  };
+
   return (
-    <div>Profile</div>
-  )
-}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
+        </label>
+        <br />
+        <label>
+          Age:
+          <input type="number" value={age} onChange={(event) => setAge(event.target.value)} />
+        </label>
+        <br />
+        <label>
+          Bio:
+          <textarea value={bio} onChange={(event) => setBio(event.target.value)} />
+        </label>
+        <br />
+        <label>
+          Profile Picture URL:
+          <input type="text" value={profilePicture} onChange={(event) => setProfilePicture(event.target.value)} />
+        </label>
+        <br />
+        <button type="submit">Save</button>
+      </form>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
